11f7ffab6eafeb3b02fea5edbe3b4ffed56feb47
Luís Jesus <lfsjesus@outlook.com>
2022-10-08 22:00:22 +0100

    Removed cmake-build-debug from all folders

--- TP1/cmake-build-debug/lib/googletest-master/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/luis/Desktop/AED_Practical-Classes/TP1/lib/googletest-master/googlemock
-# Build directory: /home/luis/Desktop/AED_Practical-Classes/TP1/cmake-build-debug/lib/googletest-master/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
